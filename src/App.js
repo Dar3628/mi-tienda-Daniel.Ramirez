41e029a1e49b844a81c1b89f2375c8b1f58d97f6
Daniel Ramirez <daniel_ramirez89@hotmail.com>
2022-01-24 14:52:48 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './loguito.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import firstReactNavbar from "./components/navbar/firstReactNavbar";
+import Navbar from "./components/navbar/firstReactNavbar";
 
 function App() {
   return (
     <div className = "App">
       <firstReactNavbar />
       <h1> Primer intento con React </h1>
-      {/* <div style={appStyles} /> */}
-    </div>
+      
+      </div>
   )
 }
 
